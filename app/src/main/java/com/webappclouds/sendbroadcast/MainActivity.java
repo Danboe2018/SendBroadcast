16e29be6174537e9a1f4863e6495e4c7d1cd831d
Dan Boehmke <Daniel@webappclouds.com>
2019-10-10 16:13:23 -0400

    Setup broadcast

--- app/src/main/java/com/webappclouds/sendbroadcast/MainActivity.java
+++ app/src/main/java/com/webappclouds/sendbroadcast/MainActivity.java
@@ -1,5 +1,6 @@
 package com.webappclouds.sendbroadcast;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -18,18 +19,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
+
+    public void sendOutBroadcast(View view){
+        Intent i = new Intent();
+        i.setAction("com.webappclouds.sendbroadcast");
+        i.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        sendBroadcast(i);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
